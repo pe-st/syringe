fda5edf737c4376e9ece4602118d9395126ea6b4
Peter Steiner <unistein+reinette@gmail.com>
2019-02-26 08:32:56 +0100

    Modify logging levels

--- syringe-core/src/main/java/ch/schlau/pesche/syringe/core/extension/SyringeExtension.java
+++ syringe-core/src/main/java/ch/schlau/pesche/syringe/core/extension/SyringeExtension.java
@@ -22,22 +22,29 @@ public class SyringeExtension implements Extension {
     // events in the order specified in https://docs.jboss.org/cdi/spec/2.0/cdi-spec.html#initialization
 
     public void beforeBeanDiscoveryEvent(@Observes BeforeBeanDiscovery beforeBeanDiscovery) {
-        logger.info("BeforeBeanDiscovery");
+        // WELD logging:
+        // DEBUG org.jboss.weld.BootstrapTracker - START bootstrap > startInitialization > BeforeBeanDiscovery
+        logger.trace("BeforeBeanDiscovery");
     }
 
     public void processAnnotatedTypeEvent(@Observes ProcessAnnotatedType processAnnotatedType) {
+        // not logged by WELD
         logger.info("Type discovery: {}", processAnnotatedType.getAnnotatedType());
     }
 
     public void afterTypeDiscoveryEvent(@Observes AfterTypeDiscovery afterTypeDiscovery) {
-        logger.info("AfterTypeDiscovery");
+        // WELD logging:
+        // DEBUG org.jboss.weld.BootstrapTracker - START bootstrap > startInitialization > AfterTypeDiscovery
+        logger.trace("AfterTypeDiscovery");
     }
 
     public void processInjectionPointEvent(@Observes ProcessInjectionPoint processInjectionPoint) {
+        // not logged by WELD
         logger.info("Bean discovery; injection point {}", processInjectionPoint.getInjectionPoint());
     }
 
     public void processInjectionTargetEvent(@Observes ProcessInjectionTarget processInjectionTarget) {
+        // usually just logged before ProcessBeanAttributes
         logger.info("Bean discovery; injection target {}", processInjectionTarget.getInjectionTarget());
     }
 
@@ -50,15 +57,24 @@ public class SyringeExtension implements Extension {
     }
 
     public void processBeanAttributesEvent(@Observes ProcessBeanAttributes processBeanAttributes) {
-        logger.info("Bean discovery; bean attributes {}", processBeanAttributes.getBeanAttributes());
+        // indirectly logged by Weld ProbeExtension (development mode):
+        // DEBUG org.jboss.weld.Bootstrap - WELD-000149: ProcessBeanAttributes.setBeanAttributes() called by org.jboss.weld.probe.ProbeExtension@b5942961:
+        //       Managed Bean [class X] with qualifiers [@Any @Default] replaced by Managed Bean [class X] with qualifiers [@Any @Default]
+        // DEBUG org.jboss.weld.probe.Probe - PROBE-000009: @MonitoredComponent stereotype added to [BackedAnnotatedType] @Dependent class X
+        logger.debug("Bean discovery; bean attributes {}", processBeanAttributes.getBeanAttributes());
     }
 
     public void processBeanEvent(@Observes ProcessBean processBean) {
-        logger.info("Bean discovery; bean {}", processBean.getBean());
+        // WELD logging depending on type:
+        // DEBUG org.jboss.weld.Bootstrap - WELD-000106: Bean: x
+        // DEBUG org.jboss.weld.Bootstrap - WELD-000107: Interceptor: x
+        logger.trace("Bean discovery; bean {}", processBean.getBean());
     }
 
     public void afterBeanDiscoveryEvent(@Observes AfterBeanDiscovery afterBeanDiscovery) {
-        logger.info("AfterBeanDiscovery");
+        // WELD logging:
+        // DEBUG org.jboss.weld.BootstrapTracker - START bootstrap > deployBeans > AfterBeanDiscovery
+        logger.trace("AfterBeanDiscovery");
     }
 
     public void afterDeploymentValidationEvent(@Observes AfterDeploymentValidation afterDeploymentValidation) {
